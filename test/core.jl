d16a1412ecdd5839e9cc9932d3cf7796b52a7e0e
Jameson Nash <vtjnash@gmail.com>
2017-05-10 15:57:32 -0400

    replace current_module() with @__MODULE__
    passes macros a __module__ argument, like the __source__ argument,
    allowing them to inspect and configure themselves based on their evaluation context
    
    remove current_module dependence from `include`:
    instead, we use the same trick as for `eval` (they are basically the same function after all)
    to create a module-relative function in each non-bare module
    
    now puts Module in MethodInstance.def for toplevel thunks to make it convenient to pass around this contextual information

--- test/core.jl
+++ test/core.jl
@@ -454,7 +454,7 @@ begin
     function f7234_b()
         global f7234_cnt += 1
         glob_x2 += 1
-        global f7235_cnt += -10000
+        global f7234_cnt += -10000
     end
 end
 @test_throws UndefVarError f7234_b()
@@ -577,8 +577,8 @@ end
         bar21900 = foo21900 + 1
     end
 end
-@test !isdefined(:foo21900)
-@test !isdefined(:bar21900)
+@test !@isdefined(foo21900)
+@test !@isdefined(bar21900)
 bar21900 = 0
 @test_throws UndefVarError @eval begin
     for i21900 = 1:10
@@ -590,7 +590,7 @@ bar21900 = 0
     end
 end
 @test bar21900 == -1
-@test !isdefined(:foo21900)
+@test !@isdefined foo21900
 foo21900 = 0
 @test nothing === @eval begin
     for i21900 = 1:10
@@ -832,6 +832,7 @@ end
 let didthrow =
     try
         include_string(
+            @__MODULE__,
             """
             module TestInitError
                 __init__() = error()
@@ -1506,7 +1507,8 @@ let
     g4505{X}(::X) = 0
     @test g4505(0) == 0
 end
-@test !isdefined(:g4505)
+@test !@isdefined g4505
+@test !isdefined(@__MODULE__, :g4505)
 
 # issue #4681
 # ccall should error if convert() returns something of the wrong type
@@ -1804,9 +1806,15 @@ test5536(a::Union{Real, AbstractArray}) = "Non-splatting"
 @test test5536(5) == "Non-splatting"
 
 # multiline comments (#6139 and others raised in #6128) and embedded NUL chars (#10994)
-@test 3 == include_string("1 + 2") == include_string("1 + #==# 2") == include_string("1 + #===# 2") == include_string("1 + #= #= blah =# =# 2") == include_string("1 + #= #= #= nested =# =# =# 2") == include_string("1 + #= \0 =# 2")
-@test_throws LoadError include_string("#=")
-@test_throws LoadError include_string("#= #= #= =# =# =")
+@test 3 ==
+    include_string(@__MODULE__, "1 + 2") ==
+    include_string(@__MODULE__, "1 + #==# 2") ==
+    include_string(@__MODULE__, "1 + #===# 2") ==
+    include_string(@__MODULE__, "1 + #= #= blah =# =# 2") ==
+    include_string(@__MODULE__, "1 + #= #= #= nested =# =# =# 2") ==
+    include_string(@__MODULE__, "1 + #= \0 =# 2")
+@test_throws LoadError include_string(@__MODULE__, "#=")
+@test_throws LoadError include_string(@__MODULE__, "#= #= #= =# =# =")
 
 # issue #6142
 import Base: +
@@ -1922,7 +1930,7 @@ macro m20524(ex)
 end
 @m20524 ((a,(b20524,c)) = (8,(1,5)); (a,b20524,c))
 @test f20524() === (8,1,5)
-@test !isdefined(:b20524)  # should not assign to a global
+@test !@isdefined b20524 # should not assign to a global
 
 # issue #6387
 primitive type Date6387{C} 64 end
@@ -3840,7 +3848,7 @@ let
     k15283 = j15283+=1
 end
 @test j15283 == 1
-@test !isdefined(:k15283)
+@test !@isdefined k15283
 
 # issue #15264
 module Test15264
